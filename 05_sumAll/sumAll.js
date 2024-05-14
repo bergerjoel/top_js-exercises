2dd814d694af998eab37336f9708d8e3c3866a94
Joël Berger <169206179+bergerjoel@users.noreply.github.com>
2024-05-14 09:15:22 +0200

    Refactor: solving exercises 03-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // Some Error-handling;
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+
+    // Fix: If min > max --> switch "min" to "max" and assign a temporary "temp" to the actual "max"
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
+    // Initialise a variable that holds the total sum;
+    let sum = 0
+
+    // looping thru, compare the two arguments, and sum them up;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
