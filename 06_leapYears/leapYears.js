fd742f22f693d8a75b13a849ad34aeae02913e37
Joël Berger <169206179+bergerjoel@users.noreply.github.com>
2024-05-16 14:56:30 +0200

    Refactor: solving and testing exercises 05-08

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // Some Error-handling;
+  if (!Number.isInteger(year)) return "ERROR";
 
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else if (year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+// Or, more simply:
+// const leapYears = function (year) {
+//     return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+//   };
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// Testing it using coderunner;
+console.log(convertToCelsius(66));
+
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
+
+// Testing it using coderunner;
+console.log(convertToFahrenheit(30));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
